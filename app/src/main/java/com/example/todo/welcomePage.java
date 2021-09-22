4637bddb8368ac2e5cb1763531a19b0d5d32913b
WasundaraHerath <wasundaraherath777@gmail.com>
2021-09-22 16:00:30 +0530

    customer handling

---
+++ app/src/main/java/com/example/todo/welcomePage.java
@@ -0,0 +1,28 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class welcomePage extends AppCompatActivity {
+
+    Button welcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+
+        welcome=findViewById(R.id.btn_customer);
+        welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(welcomePage.this,LoginActivity.class));
+            }
+        });
+    }
+}
